6d9fe87af4f5bebcb2f84f2c5635df27a00c835a
Mohammed0Shnan <mohammed0shnan@gmail.com>
2024-02-05 11:43:35 -0800

    third

--- src/mock/experience/index.ts
+++ src/mock/experience/index.ts
@@ -27,9 +27,18 @@ export const EXPERIENCES: Experience[] = [
     ],
     skills: [
       { label: SkillsEnum.Flutter },
-
+      { label: SkillsEnum.Dart },
+      { label: SkillsEnum.Bloc },
+      { label: SkillsEnum.FireBase },
+      { label: SkillsEnum.FireStorage },
+      { label: SkillsEnum.Github },
+      { label: SkillsEnum.RestAPI },
+      { label: SkillsEnum.Responsive },
+      { label: SkillsEnum.RealTimeDatabase },
+      { label: SkillsEnum.GraphQL },
+      { label: SkillsEnum.Testing },
     ],
-    assetURL: "/assets/images/th.jpg",
+    assetURL: "/assets/images/flutter.svg",
   },
 
   //
@@ -61,11 +70,101 @@ export const EXPERIENCES: Experience[] = [
     ],
     skills: [
       { label: SkillsEnum.Flutter },
+      { label: SkillsEnum.Dart },
+      { label: SkillsEnum.Bloc },
+      { label: SkillsEnum.FireBase },
+      { label: SkillsEnum.FireStorage },
+      { label: SkillsEnum.RealTimeDatabase },
+      { label: SkillsEnum.Stripe },
+      { label: SkillsEnum.GraphQL },
+      { label: SkillsEnum.Testing },
+      { label: SkillsEnum.Publishing },
+      { label: SkillsEnum.Support },
       { label: SkillsEnum.Javascript },
       { label: SkillsEnum.Typescript },
+      { label: SkillsEnum.CloudFunction },
       { label: SkillsEnum.NodeJs },
 
     ],
-    assetURL: "/assets/images/web-development-2.png",
+    assetURL: "/assets/images/cloud.png",
+  },
+  {
+    position: "Flutter Developer",
+    company: { name: "Yes Soft", link: "https://ae.linkedin.com/company/yes-soft" },
+    startDate: {
+      date: moment({
+        year: 2019,
+        month: 1,
+        day: 1,
+      })
+        .utc()
+        .toDate(),
+      isPresent: false,
+    },
+    endDate: {
+      date: moment({
+        year: 2022,
+        month: 0,
+        day: 1,
+      }).toDate(),
+      isPresent: false,
+    },
+    responsibilities: [
+      "Mobile Application Developer",
+      "Architectural System Design",
+      "Unit Testing And Integration Testing"
+    ],
+    skills: [
+      { label: SkillsEnum.Flutter },
+      { label: SkillsEnum.Dart },
+      { label: SkillsEnum.Bloc },
+      { label: SkillsEnum.FireBase },
+      { label: SkillsEnum.FireStorage },
+      { label: SkillsEnum.RealTimeDatabase },
+      { label: SkillsEnum.Testing },
+
+    ],
+    assetURL: "/assets/images/flutter.svg",
+  },
+  {
+    position: "Spring boot Developer",
+    company: { name: "B-Wire", link: "https://www.b-wire.co/" },
+    startDate: {
+      date: moment({
+        year: 2018,
+        month: 8,
+        day: 1,
+      })
+        .utc()
+        .toDate(),
+      isPresent: false,
+    },
+    endDate: {
+      date: moment({
+        year: 2021,
+        month: 0,
+        day: 1,
+      }).toDate(),
+      isPresent: false,
+    },
+    responsibilities: [
+      "Fulfilling non-functional and functional requirements",
+      "Building microservices And system distribution"
+    ],
+    skills: [
+      { label: SkillsEnum.SpringBoot },
+      { label: SkillsEnum.Java },
+      { label: SkillsEnum.SQL },
+      { label: SkillsEnum.Microservice },
+      { label: SkillsEnum.RabbitMQ },
+      { label: SkillsEnum.Testing },
+      { label: SkillsEnum.Caching },
+      { label: SkillsEnum.Security },
+      { label: SkillsEnum.RestAPI },
+      { label: SkillsEnum.Docker },
+
+
+    ],
+    assetURL: "/assets/images/spring.png",
   },
 ];
